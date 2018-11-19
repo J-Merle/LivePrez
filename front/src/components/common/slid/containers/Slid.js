ea63e36782640226a743cf919539fea4d3ca9950
A.Dugelet <du.aubin@hotmail.fr>
2018-11-19 16:11:24 +0100

    building middle part

---
+++ front/src/components/common/slid/containers/Slid.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './slid.css';
+import '../../../../lib/bootstrap-3.3.7-dist/css/bootstrap.min.css';
+import Content from '../../content/containers/Content';
+import EditMetaSlid from '../components/EditMetaSlid';
+
+export default class Slid extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            thumbnail: this.props.contentMap['temp']
+        }
+    }
+
+    render() {
+        let editForm;
+        if (this.props.displayMode === 'FULL_MNG') {
+            editForm = <EditMetaSlid title={this.props.title} txt={this.props.txt} />
+        }
+        return (
+            <div className="slid">
+                <div>
+                    <h1>{this.props.title}</h1>
+                    <p>{this.props.txt}</p>
+                    <Content id={this.state.thumbnail.id} src={this.state.thumbnail.src} title={this.state.thumbnail.title}
+                        type={this.state.thumbnail.type} onlyContent={true} />
+                </div>
+                {editForm}
+            </div>
+        );
+    }
+}
--- front/src/components/mainPanel/Main.js
+++ front/src/components/mainPanel/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './main.css';
 import '../../lib/bootstrap-3.3.7-dist/css/bootstrap.min.css';
-import Content from '../common/content/containers/Content';
+import Slid from '../common/slid/containers/Slid';
 import BrowseContentPanel from '../browseContentPanel/containers/BrowseContentPanel';
 import * as contentMapTmp from '../../source/contentMap.json';
 
@@ -22,7 +22,10 @@ export default class Main extends React.Component {
                         Left
                     </div>
                     <div className='col-md-6 col-lg-6 height-100 vertical-scroll'>
-                        Middle
+                        <Slid id={0} title="This is my first slid" txt="This really need more content like, fr i mean, well, you know."
+                            content_id={1} contentMap={this.state.contentMap} displayMode="FULL_MNG"/>
+                        <Slid id={2} title="This is my second slid" txt="This was is even shorter, because its displayMode is SHORT"
+                            content_id={3} contentMap={this.state.contentMap} displayMode="SHORT"/>
                     </div>
                     <div className='col-md-3 col-lg-3 height-100 vertical-scroll'>
                         <BrowseContentPanel contentMap={this.state.contentMap}/>
