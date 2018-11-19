b02fd3d07c7c4bec1a29179a6a6d882fd56cede4
Jeremy Merle <jeremy.merle@protonmail.com>
2018-11-19 09:40:27 +0100

    Fix ContentModel constructor

--- Node/app/models/content.model.js
+++ Node/app/models/content.model.js
@@ -4,57 +4,75 @@ let CONFIG = JSON.parse(process.env.CONFIG);
 const fs = require('fs');
 
 
-
 function ContentModel(content){
+  this.type = null;
+  this.id = null;
+  this.title = null;
+  this.src = null;
+  this.fileName = null;
+  this.updateContent(content);
+}
+
+ContentModel.prototype.updateContent = function(content) {
+  if (content !== undefined) {
     this.type = content.type;
     this.id = content.id;
     this.title = content.title;
     this.src = content.src;
     this.fileName = content.fileName;
+  }
+}
+
+ContentModel.prototype.setData = function(data) {
+  this.data = data;
+}
+
+ContentModel.prototype.getData = function(data) {
+  return this.data;
 }
 
 ContentModel.create =  function(content, callback) {
-    let path= '';
-    if(this.type === 'img') {
-        path = `${CONFIG.contentDirectory}/${content.fileName}`;
-    } else {
-        path = `${CONFIG.contentDirectory}/${content.id}.mesa.json`;
+  let path= '';
+  if(this.type === 'img') {
+    path = `${CONFIG.contentDirectory}/${content.fileName}`;
+  } else {
+    path = `${CONFIG.contentDirectory}/${content.id}.mesa.json`;
+  }
+
+  fs.writeFile(path, JSON.parse(content), function(err) {
+    if(err) {
+      return console.log(err);
     }
-
-    fs.writeFile(path, JSON.parse(content), function(err) {
-        if(err) {
-            return console.log(err);
-        }
-    });
+  });
 }
 
- ContentModel.read = function(id, callback) {
-    var path = `${CONFIG.contentDirectory}/${id}.mesa.json`;
-    fs.readFile(path, function(err, data){
-        if(err) {
-            return console.log(err);
-        }
-        return data;
-    });
+ContentModel.read = function(id, callback) {
+  var path = `${CONFIG.contentDirectory}/${id}.mesa.json`;
+  fs.readFile(path, function(err, data){
+    if(err) {
+      return console.log(err);
+    }
+    return data;
+  });
 }
 
- ContentModel.update = function(content, callback) {}
+ContentModel.update = function(content, callback) {}
 
- ContentModel.delete = function(id, callback) {
+ContentModel.delete = function(id, callback) {
 
-    var path = `${CONFIG.contentDirectory}/${id}.mesa.json`;
-    fs.readFile(path, function(err){
-        if(err) {
-            return console.log(err);
-        }
-    });
+  var path = `${CONFIG.contentDirectory}/${id}.mesa.json`;
+  fs.readFile(path, function(err){
+    if(err) {
+      return console.log(err);
+    }
+  });
 
-    path = `${CONFIG.contentDirectory}/${this.fileName}`;
-    fs.readFile(path, function(err){
-        if(err) {
-            return console.log(err);
-        }
-    });
+  path = `${CONFIG.contentDirectory}/${this.fileName}`;
+  fs.readFile(path, function(err){
+    if(err) {
+      return console.log(err);
+    }
+  });
 }
 
 module.exports = ContentModel;
